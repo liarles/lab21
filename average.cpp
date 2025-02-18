ad51180bc1b4027aa8c05efb8ec60067d9fa64b2
liarles <62631264+liarles@users.noreply.github.com>
2025-02-18 15:06:18 +0700

    Update average.cpp

--- average.cpp
+++ average.cpp
@@ -1,1 +1,19 @@
+#include<iostream>
+#include<cstdlib>
 
+using namespace std;
+
+int main(int argc,char *argv[]){
+    int sum;
+    if (argc == 1){
+    cout << "Please input numbers to find average.\n";
+    }else{
+        for (int i = 0 ; i < argc ; i ++){
+        sum += atof(argv[i]);
+        }
+        cout << "---------------------------------\n";
+        cout << "Average of " << argc-1 << " numbers = " << sum/(argc-1);
+        cout << "\n---------------------------------";
+    }
+    return 0;
+}
